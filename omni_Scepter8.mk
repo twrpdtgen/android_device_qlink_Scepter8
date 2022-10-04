05302d39c9b956fd3206ec9927037bbd90283f66
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-10-04 12:35:08 +0200

    Scepter8: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: qlink Model: Scepter8 Script version: 3.0.0

---
+++ omni_Scepter8.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from Scepter8 device
+$(call inherit-product, device/qlink/Scepter8/device.mk)
+
+PRODUCT_DEVICE := Scepter8
+PRODUCT_NAME := omni_Scepter8
+PRODUCT_BRAND := Qlink
+PRODUCT_MODEL := Scepter8
+PRODUCT_MANUFACTURER := qlink
+
+PRODUCT_GMS_CLIENTID_BASE := android-along
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="Scepter8-user 12 SQ1A.220105.002 20220802.143720 release-keys"
+
+BUILD_FINGERPRINT := Qlink/Scepter8/Scepter8:12/SQ1A.220105.002/20220802:user/release-keys
