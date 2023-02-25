335befa999560ab3fcc1a58fafca69f1b984af4b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-02-25 21:52:07 +0100

    Scepter8: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: qlink Model: Scepter8 Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_Scepter8.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_Scepter8-user \
+    omni_Scepter8-userdebug \
+    omni_Scepter8-eng
---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_Scepter8-user
+add_lunch_combo omni_Scepter8-userdebug
+add_lunch_combo omni_Scepter8-eng
